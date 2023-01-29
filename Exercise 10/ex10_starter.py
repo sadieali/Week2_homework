36ae28d6e6ce49c9fc088f345eaf4396b5407d63
Jessica Wan <j27wan@gmail.com>
2023-01-29 20:18:17 +0000

    Comments added to Exercise 10 starter file

--- Exercise 10/ex10_starter.py
+++ Exercise 10/ex10_starter.py
@@ -13,12 +13,16 @@ pattern = os.path.join(hdir,'*')
 fileNames = (glob.glob(pattern))
 
 # TODO: use os.path.getsize to find each file's size
+# each_element represents items in fileNames list
+# os.path.getsize is a function which finds the size of the item and stores it in variable fileSizes
 for each_element in fileNames:
     fileSizes = os.path.getsize(each_element)
 # TODO: Add a test to only display files that are not zero length
+# if the size of the file is not 0, the size will be printed alongside the name of the file and its name
+# else condition not required as nothing needs to happen if file size is 0
     if fileSizes != 0:
         print(fileSizes)
 # TODO: Remove the leading directory name(s) from each filename before you print it - 
-# using os.path.basename()
-        fileShortName = os.path.basename(each_element)
-        print(fileShortName)
+# using os.path.basename() the leading directory removed and the name is stored in fileBaseName variable
+        fileBaseName = os.path.basename(each_element)
+        print(fileBaseName)
