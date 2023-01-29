f9ee6d02d7b4b70e94270c3c30f3d04a72a71453
Jessica Wan <j27wan@gmail.com>
2023-01-29 14:17:07 +0000

    Exercise 10 Starter completed, file updated

--- Exercise 10/ex10_starter.py
+++ Exercise 10/ex10_starter.py
@@ -10,11 +10,15 @@ else:
 pattern = os.path.join(hdir,'*')
 
 # TODO: Use the glob.glob() function to obtain the list of filenames
-print(glob.glob(pattern))
+fileNames = (glob.glob(pattern))
+
 # TODO: use os.path.getsize to find each file's size
-
+for each_element in fileNames:
+    fileSizes = os.path.getsize(each_element)
 # TODO: Add a test to only display files that are not zero length
-
+    if fileSizes != 0:
+        print(fileSizes)
 # TODO: Remove the leading directory name(s) from each filename before you print it - 
 # using os.path.basename()
-
+        fileShortName = os.path.basename(each_element)
+        print(fileShortName)
--- Exercise 10/supplied_pin.py
+++ Exercise 10/supplied_pin.py
@@ -1,27 +1,25 @@
-correct_pin = 4321
+# variable to identify correct pin
+# constant values at top of the code for cleaner and more maintainable code
+correct_pin = "4321"
 
-login = [1, 2, 3]
+login = [0, 1, 2]
 # login is a list shown by square brackets, is mutable which allows pop() method
+# checks how many failed login attempts
 
+# while, if and else are conditions
+# while login is true (while the list contains items), an input will be requested
 while login:
     supplied_pin = input('Enter your pin: ')
+
+    # if the supplied pin variable is equal (==) to the correct pin variable, the loop will end here
+    # break is a loop control statement to trigger loop termination
     if supplied_pin == correct_pin:
         print('Successful login')
+        break
+
+    # else (if the pin is incorrect),
+    # pop() is a method which removes the last item from the list after a failed login
+    # str converts the login integer to string to allow concatenation (using +) with string
     else:
         print('Failed login')
         print(str(login.pop()) + ' Attempts remaining')
-
-# while, if and else are conditions
-# pop() is a method which removes the last item from the list
-# str convert the login integer to string to allow concatenation (using +) with string
-
-# def pin_num():
-#     attempt= 0
-#     while attempt < 3:
-#         num = input('Please Enter Your 4 Digit Pin: ')
-#         if enter_pin(pinnum):
-#             print("Pin accepted!")
-#         else:
-#             print("Invalid pin")
-#             attempt += 1
-#             print("Multiple attempts, Your card is blocked, please contact customer service")
