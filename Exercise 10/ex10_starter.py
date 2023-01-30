145d2230575a02e51176c3b634f3e0213874cc2c
Jessica Wan <j27wan@gmail.com>
2023-01-30 17:50:56 +0000

    Comments updated to explain Ex.10 more clearly

--- Exercise 10/ex10_starter.py
+++ Exercise 10/ex10_starter.py
@@ -7,7 +7,7 @@ else:
     hdir = os.environ['HOME']
 
 # Construct a portable wildcard pattern
-pattern = os.path.join(hdir,'*')
+pattern = os.path.join(hdir, '*')
 
 # TODO: Use the glob.glob() function to obtain the list of filenames
 fileNames = (glob.glob(pattern))
@@ -15,6 +15,8 @@ fileNames = (glob.glob(pattern))
 # TODO: use os.path.getsize to find each file's size
 # each_element represents items in fileNames list
 # os.path.getsize is a function which finds the size of the item and stores it in variable fileSizes
+# for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string)
+# in this case, it is incrementing each element in the fileNames list
 for each_element in fileNames:
     fileSizes = os.path.getsize(each_element)
 # TODO: Add a test to only display files that are not zero length
@@ -24,5 +26,6 @@ for each_element in fileNames:
         print(fileSizes)
 # TODO: Remove the leading directory name(s) from each filename before you print it - 
 # using os.path.basename() the leading directory removed and the name is stored in fileBaseName variable
+# nested this function so that only files larger than 0 will be printed
         fileBaseName = os.path.basename(each_element)
         print(fileBaseName)
